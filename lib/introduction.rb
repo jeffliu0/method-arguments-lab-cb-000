24067606bfdedebba7446f3401994ff528bf8169
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-10 04:20:08 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
 def introduction(name, language)
-  "Hi. My name is #{name} and I am learning to program in #{language}."
+  put "Hi. My name is #{name} and I am learning to program in #{language}."
 end
